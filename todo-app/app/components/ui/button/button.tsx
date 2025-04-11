1a8b87286165951ed7b97e175e04bc8c961ee5f1
Ahadjie-Bless <bless.ahadjie@amalitech.com>
2025-04-11 20:27:44 +0000

    feat(frontend): implement button for reusability sake

---
+++ todo-app/app/components/ui/button/button.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import {ButtonProps} from "@/app/components/ui/button/type";
+import React from "react";
+
+const Button: React.FC<ButtonProps> = ({children, onClick, type = 'button', disabled = false,
+                                           className = '',
+                                           color = 'white',
+                                           bgColor = 'blue-500',
+                                           width = 'auto',
+                                           height = 'auto',
+                                           borderRadius = '0.25rem',
+                                           border = 'none',
+                                           fontSize = '1rem',
+                                           fontWeight = 'normal',
+                                           hoverColor = color,
+                                           hoverBgColor = bgColor,
+                                           padding = '1px',
+                                           ...props
+                                       }) => {
+
+    const formatSize = (value: string | number) =>
+        typeof value === 'number' ? `${value}px` : value;
+
+    return (
+        <button
+            type={type}
+            onClick={onClick}
+            disabled={disabled}
+            className={`${className} transition-colors duration-200`}
+            style={{
+                color,
+                backgroundColor: bgColor,
+                width: formatSize(width),
+                height: formatSize(height),
+                borderRadius: formatSize(borderRadius),
+                border,
+                fontSize: formatSize(fontSize),
+                fontWeight,
+                padding: formatSize(padding),
+
+                ['--hover-color' as any]: hoverColor,
+                ['--hover-bg-color' as any]: hoverBgColor,
+            }}
+            {...props}
+        >
+            {children}
+        </button>
+    );
+};
+
+export default Button;
