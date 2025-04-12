b4afdd57abd4961a301dc80c736a47ba66a23cf4
Ahadjie-Bless <bless.ahadjie@amalitech.com>
2025-04-12 11:27:05 +0000

    feat(frontend): update validation

--- todo-app/app/libs/login-form.ts
+++ todo-app/app/libs/login-form.ts
@@ -1,4 +1,14 @@
-export const validateForm = (email: string, password: string) => {
+export const validateLoginForm = (email: string, password: string) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email) && password.length >= 6;
+    return emailRegex.test(email) && password.length >= 8;
+};
+
+export const validateRegisterForm = (username: string, email: string, password: string, confirmPassword: string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return (
+        username.length > 0 &&
+        emailRegex.test(email) &&
+        password.length >= 8 &&
+        password === confirmPassword
+    );
 };
