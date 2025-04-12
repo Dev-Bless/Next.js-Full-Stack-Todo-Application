3e5ab8664b3c2216c6768fc1f7dd13f7a7457375
Ahadjie-Bless <bless.ahadjie@amalitech.com>
2025-04-12 01:35:31 +0000

    feat(frontend): add auth reducer to store

--- todo-app/app/redux/store.ts
+++ todo-app/app/redux/store.ts
@@ -1,9 +1,14 @@
 'use client'
 import todoSliceReducer from "./slices/todoSlice";
+import authReducer from "./slices/authSlice";
 import {configureStore} from "@reduxjs/toolkit";
 
 export const store = configureStore({
     reducer: {
         todos: todoSliceReducer,
+        auth: authReducer,
     },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
